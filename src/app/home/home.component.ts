cd3d3a67df1b37206aa45e441a639bb5735e64e3
Michelle Oliveira <miolivc@outlook.com>
2018-08-17 14:13:17 -0300

    Adicionando mock de ofertas ao ofertas.service

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,15 +1,18 @@
 import { Component, OnInit } from '@angular/core';
+import { OfertasService } from '../services/ofertas.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
+  providers: [ OfertasService ]
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  constructor(private ofertasService : OfertasService) { }
 
   ngOnInit() {
+    // console.log(this.ofertasService.getOfertas());
   }
 
 }
