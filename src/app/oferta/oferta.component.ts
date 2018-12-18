b35527ed1490e756863d7d31980c4870577d63ad
Michelle Oliveira <miolivc@outlook.com>
2018-12-17 21:16:31 -0300

    Utilizando ActivatedRoute para recuperar dados do snapshow

--- src/app/oferta/oferta.component.ts
+++ src/app/oferta/oferta.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-oferta',
@@ -7,9 +8,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class OfertaComponent implements OnInit {
 
-  constructor() { }
+  constructor(private route : ActivatedRoute) { }
 
   ngOnInit() {
+    console.log( "Parametro Recebido: " + this.route.snapshot.params["id"] )
   }
 
 }
