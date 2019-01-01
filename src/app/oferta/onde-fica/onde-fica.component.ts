ae0a3fc3ee4725d5c686cd8feb32c5d4d5e483f9
Michelle Oliveira <miolivc@outlook.com>
2019-01-01 10:46:06 -0300

    Criando componentes como-usar e onde-fica

---
+++ src/app/oferta/onde-fica/onde-fica.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-onde-fica',
+  templateUrl: './onde-fica.component.html',
+  styleUrls: ['./onde-fica.component.css']
+})
+export class OndeFicaComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
