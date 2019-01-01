90f0073bf13a62065a2bed594d4053ea076eb626
Michelle Oliveira <miolivc@outlook.com>
2019-01-01 10:22:51 -0300

    Mudando base da url para atributo (uso de template string)

--- src/app/services/ofertas.service.ts
+++ src/app/services/ofertas.service.ts
@@ -5,31 +5,33 @@ import { Oferta } from '../shared/oferta.model';
 
 @Injectable()
 export class OfertasService {
-    
-    constructor(private http : Http) { }
-  
-    public getOfertas() : Promise<Oferta[]> {
-        return this.http.get( "http://localhost:3000/ofertas" )
+
+    private url : String = "http://localhost:3000/ofertas";
+
+    constructor(private http: Http) { }
+
+    public getOfertas(): Promise<Oferta[]> {
+        return this.http.get(`${this.url}`)
             .toPromise()
-            .then( ( answer : any ) => answer.json() )
+            .then((answer: any) => answer.json())
     }
 
-    public getOfertasDestaque() : Promise<Oferta[]> {
-        return this.http.get( "http://localhost:3000/ofertas?destaque=true" )
+    public getOfertasDestaque(): Promise<Oferta[]> {
+        return this.http.get(`${this.url}?destaque=true`)
             .toPromise()
-            .then( ( answer : any ) => answer.json() )
+            .then((answer: any) => answer.json())
     }
 
-    public getOfertasPorCategoria( categoria : String ) : Promise<Oferta[]> {
-        return this.http.get( `http://localhost:3000/ofertas?categoria=${categoria}` )
+    public getOfertasPorCategoria(categoria: String): Promise<Oferta[]> {
+        return this.http.get(`${this.url}?categoria=${categoria}`)
             .toPromise()
-            .then( ( answer : any ) => answer.json() )
+            .then((answer: any) => answer.json())
     }
- 
-    public getOfertasByID( id : number ) : Promise<Oferta> {
-        return this.http.get( `http://localhost:3000/ofertas?id=${id}` )
+
+    public getOfertasByID(id: number): Promise<Oferta> {
+        return this.http.get(`${this.url}?id=${id}`)
             .toPromise()
-            .then( ( answer : any ) => answer.json().shift() )
+            .then((answer: any) => answer.json().shift())
     }
 
 }
