1da70909ba678248488431c353f5a5acb093fa38
Michelle Oliveira <miolivc@outlook.com>
2019-01-01 10:15:30 -0300

    Adicionando variavel ao template de oferta

--- src/app/oferta/oferta.component.ts
+++ src/app/oferta/oferta.component.ts
@@ -4,33 +4,27 @@ import { OfertasService } from '../services/ofertas.service';
 import { Oferta } from '../shared/oferta.model';
 
 @Component({
-  selector: 'app-oferta',
-  templateUrl: './oferta.component.html',
-  styleUrls: ['./oferta.component.css'],
-  providers: [ OfertasService ]
+	selector: 'app-oferta',
+	templateUrl: './oferta.component.html',
+	styleUrls: ['./oferta.component.css'],
+	providers: [OfertasService]
 })
 export class OfertaComponent implements OnInit {
 
-  public oferta : Oferta;
+	public oferta: Oferta;
 
-  constructor(
-    private route : ActivatedRoute, 
-    private ofertasService : OfertasService
-  ) { }
+	constructor(
+		private route: ActivatedRoute,
+		private ofertasService: OfertasService
+	) { }
 
-  ngOnInit() {
-    // Snapshot Example 
-    console.log( `Parametro Recebido por Snapshot: ${this.route.snapshot.params["id"]}` ) 
+	ngOnInit() {
+		let idOferta = this.route.snapshot.params["id"];
 
-    // Subscribe Example
-    // this.route.params.subscribe( ( param : any ) => {
-    //   console.log( `Parametro passado pelo Subscribe ${param.id}` )
-	// })
-	
-	this.ofertasService.getOfertasByID(this.route.snapshot.params["id"])
-		.then( ( oferta : Oferta ) => {
-			this.oferta = oferta;
-		}) 
-  } 
+		this.ofertasService.getOfertasByID(idOferta)
+			.then((oferta: Oferta) => {
+				this.oferta = oferta;
+			})
+	}
 
 }
