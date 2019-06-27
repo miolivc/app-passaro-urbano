6c3d22b468eea874863296fb3303eed126463a3e
Michelle Oliveira <miolivc@outlook.com>
2019-06-27 05:48:04 -0300

    Criaçao de Observaador e definiçao de uma açao para o recebimento de cada evento dos objetos observados por este desse observador

--- src/app/oferta/oferta.component.ts
+++ src/app/oferta/oferta.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { OfertasService } from '../services/ofertas.service';
 import { Oferta } from '../shared/oferta.model';
-import { Observable, interval } from 'rxjs';
+import { Observable, interval, Observer } from 'rxjs';
 
 @Component({
 	selector: 'app-oferta',
@@ -27,7 +27,19 @@ export class OfertaComponent implements OnInit {
 			this.oferta = oferta;
 		})
 
-		interval(500).subscribe((intervalo : number) => console.log(intervalo));
+		// interval(500).subscribe((intervalo : number) => console.log(intervalo));
+
+		// Observável -> Objeto no qual suas mudanças serão observadas, isto é,
+		// haverá um objeto responsável por capturar esses 'eventos' de mudança e executar uma ação
+		let observableTeste = Observable.create((observer : Observer<string>) => {
+			// next realiza uma nova emissão do evento
+			observer.next('Teste de Stream -> Esta é uma mudnaça no estado do objeto a ser observado');
+		})
+
+		// Observador -> Este a cada mudança de estado dos nossos objetos observados realiza uma ação 
+		observableTeste.subscribe(
+			(texto : string) => console.log(`Texto recuperado do objeto observador: ${texto}`)
+		)
 	}
 
 }
