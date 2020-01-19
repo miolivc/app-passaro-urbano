1f92f002740dd2f36e5f2a5889139e10bd59e5a5
Michelle Oliveira <miolivc@outlook.com>
2020-01-19 00:17:43 -0300

    Corrigindo inferencia de tipo para Response (do HTTP)

--- src/app/services/ofertas.service.ts
+++ src/app/services/ofertas.service.ts
@@ -1,6 +1,6 @@
 
 import { Injectable } from '@angular/core';
-import { Http } from '@angular/http';
+import { Http, Response } from '@angular/http';
 import { Oferta } from '../shared/oferta.model';
 import { Observable } from 'rxjs';
 import { map, retry } from 'rxjs/operators';
@@ -15,43 +15,43 @@ export class OfertasService {
     public getOfertas(): Promise<Oferta[]> {
         return this.http.get(`${this.url}/ofertas`)
             .toPromise()
-            .then((answer: any) => answer.json())
+            .then((answer: Response) => answer.json())
     }
 
     public getOfertasDestaque(): Promise<Oferta[]> {
         return this.http.get(`${this.url}/ofertas?destaque=true`)
             .toPromise()
-            .then((answer: any) => answer.json())
+            .then((answer: Response) => answer.json())
     }
 
     public getOfertasPorCategoria(categoria: String): Promise<Oferta[]> {
         return this.http.get(`${this.url}/ofertas?categoria=${categoria}`)
             .toPromise()
-            .then((answer: any) => answer.json())
+            .then((answer: Response) => answer.json())
     }
 
     public getOfertasByID(id: number): Promise<Oferta> {
         return this.http.get(`${this.url}/ofertas?id=${id}`)
             .toPromise()
-            .then((answer: any) => answer.json().shift())
+            .then((answer: Response) => answer.json().shift())
     }
 
     public getComoUsarOfertaPorId(id: number) : Promise<string> {
         return this.http.get(`${this.url}/como-usar?id=${id}`)
             .toPromise()
-            .then((answer : any) => answer.json().shift().descricao)
+            .then((answer : Response) => answer.json().shift().descricao)
     }
 
     public getOndeFicaOfertaPorId(id: number) : Promise<string> {
         return this.http.get(`${this.url}/onde-fica?id=${id}`)
             .toPromise()
-            .then((answer : any) => answer.json().shift().descricao)
+            .then((answer : Response) => answer.json().shift().descricao)
     }
 
     public pesquisarOfertas(termo : string) : Observable<Oferta[]> {
         return this.http.get(`${this.url}/ofertas/?descricao_oferta_like=${termo}`)
             .pipe(
-                map((response : any) => response.json()),
+                map((response : Response) => response.json()),
                 retry(10)  // Efetua 10 tentativas antes de exibir o erro
             )
     }
